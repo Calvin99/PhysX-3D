ef1ecfecb47130ec3aefda93f18b981d6cabedfa
Calvin McHenry <mchenrycalvin@gmail.com>
2020-03-09 15:17:59 -0700

    Removed initial testing blocks

--- js/physx.js
+++ js/physx.js
@@ -9,6 +9,7 @@ const Height = canvas.height;
 var grid = new Grid(10);
 var mouse = new Mouse();
 
+/*
 grid.set(0, 0, 0, Properties.Water);
 grid.set(1, 0, 0, Properties.Water);
 grid.set(0, 1, 0, Properties.Water);
@@ -24,9 +25,7 @@ grid.set(9, 9, 7, Properties.Sand);
 grid.set(9, 9, 6, Properties.Sand);
 grid.set(9, 9, 5, Properties.Sand);
 grid.set(9, 9, 4, Properties.Sand);
-
-/*for (i = 0; i < grid.scale; ++i)
-	grid.set(grid.scale - 1, grid.scale - 1, i, Properties.Glass);*/
+*/
 
 setInterval(draw, 40);
 function draw() {
